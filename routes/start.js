a44134c6a6948c3ae43dc5d8043510769ca5fa18
Elroy Rebello <elroyrebello2015@gmail.com>
2021-08-03 11:33:57 +0530

    done

---
+++ routes/start.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const File = require('../models/file');
+
+router.get('/', async (req, res) => {
+    
+        
+            return res.render('index');
+      
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 // Cors 
 const corsOptions = {
   origin: process.env.ALLOWED_CLIENTS.split(',')
-  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+   ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
 }
 
 // Default configuration looks like
@@ -33,6 +33,6 @@ app.set('view engine', 'ejs');
 app.use('/api/files', require('./routes/files'));
 app.use('/files', require('./routes/show'));
 app.use('/files/download', require('./routes/download'));
-
+app.use('/',require('./routes/start'))
 
 app.listen(PORT, console.log(`Listening on port ${PORT}.`));
